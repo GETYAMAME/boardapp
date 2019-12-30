86ba0617e6a856c62285252cf9356971c078ff78
GETYAMAME <pon.mikan10@gmail.com>
2019-12-31 04:21:32 +0900

    first commit

---
+++ src/graphql/mutations.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createPost = `mutation CreatePost(
+  $input: CreatePostInput!
+  $condition: ModelPostConditionInput
+) {
+  createPost(input: $input, condition: $condition) {
+    id
+    title
+    content
+    price
+    rating
+  }
+}
+`;
+export const updatePost = `mutation UpdatePost(
+  $input: UpdatePostInput!
+  $condition: ModelPostConditionInput
+) {
+  updatePost(input: $input, condition: $condition) {
+    id
+    title
+    content
+    price
+    rating
+  }
+}
+`;
+export const deletePost = `mutation DeletePost(
+  $input: DeletePostInput!
+  $condition: ModelPostConditionInput
+) {
+  deletePost(input: $input, condition: $condition) {
+    id
+    title
+    content
+    price
+    rating
+  }
+}
+`;
